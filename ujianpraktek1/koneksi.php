8e7c348151167e781a274af36667fb39acd7c2b7
tarmidzibariq <81232207+tarmidzibariq@users.noreply.github.com>
2021-03-25 17:49:17 +0700

    Delete ujianpraktek1 directory

--- ujianpraktek1/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host       ="localhost";
-$user       ="root";
-$password   ="";
-$database   ="ujiprak";
-$connect    =mysqli_connect($host,$user,$password,$database) or die("Gagal Menghubungkan");
-?>
